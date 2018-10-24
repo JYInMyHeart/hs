7d6455afbab20f4cc7cb63cd84ada5467575376d
xuchangkun <xuchangkun@leadeon.cn>
2018-10-24 18:11:57 +0800

    type system

--- simple/src/iteratee1.hs
+++ simple/src/iteratee1.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE DeriverFunctor #-}
 
-import Data.Function(fix)
-import Control.Monad
-import qualified Control.Exception as Exc
-import Control.Monad.IO.Class
-import Control.Monad.Trans
+import qualified Control.Exception      as Exc
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans
+import           Data.Function          (fix)
 
 data Stream a = Chunks [a] | EOF deriving (Show,Eq,Functor)
 
 instance Monoid (Stream a) where
   mempty = Chunks mempty
   mappend (Chunks xs) (Chunks ys) = Chunks (xs ++ ys)
-  mappend _ _ = EOF
+  mappend _ _                     = EOF
 
 instance Monad Stream where
   return = Chunks . return
   Chunks xs >>= f = mconcat (fmap f xs)
   EOF >>= _ = EOF
+
+--  liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+-- (<$>) :: Functor f => (a -> b) -> f a -> f b
+-- liftA2 (<$>) :: (a1 -> b1) -> (f1 a1 -> f1 b1) -> f (a1 -> b1) -> f f1 a1 -> f f1 b1
---
+++ simple/src/rank.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE Rank2Types #-}
+import Control.Monad.ST
+import Data.STRef
+
+factorial :: Int -> STRef s Int -> ST s Int
+factorial n accRef = do
+  numRef <- newSTRef n
+  num <- readSTRef numRef
+  if num < 1
+    then readSTRef accRef
+    else do
+      acc <- readSTRef accRef
+      writeSTRef accRef (acc * n)
+      writeSTRef numRef (num - 1)
+      factorial (num - 1) accRef
+
+fact :: Int -> Int
+fact n = 
+  runST $ do
+    accRef <- new STRef 1
+    factorial n accRef
+
+foo :: (forall a . [a] -> [a]) -> ([b],[c]) -> ([b],[c])
+foo f (xs,ys) = (f xs,f ys)
+
+main = print $ foo (take 2) ([1,2,3],"456")
+
