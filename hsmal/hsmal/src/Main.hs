82a24cb2a4a86bbb76463794f13a229444f1df05
alex <changhuxxu@gmail.com>
2018-12-13 00:02:03 +0800

    repl
    Signed-off-by: alex <changhuxxu@gmail.com>

--- hsmal/hsmal/src/Main.hs
+++ hsmal/hsmal/src/Main.hs
@@ -1,25 +1,24 @@
 module Main where
 
-import           Context
-import           Evaluator
-import           Parser
-import           Syntax
-import           TypeChecker
-import           System.Environment
-import           Text.Parsec
+import Context
+import Evaluator
+import Parser
+import Syntax
+import System.Environment
+import Text.Parsec
+import TypeChecker
 
 main :: IO ()
-main =
-  do putStr "user> "
-     args <- getLine
-     case args of
-       sourceFile ->
-         do let ctx = mkContext
-            parseTree <- fmap (runParser parseTerm ctx "untyped") (readFile $ "../test/" ++ sourceFile ++ ".txt")
-            case  parseTree of
-              Right expr -> 
-                case (typeOf ctx expr) of
-                  Right ex -> putStrLn $ "Evaluating " ++ (show parseTree) ++ "\n=> " ++ (showTerm ctx . eval) expr
-                  Left err -> putStrLn $ "Type Error: " ++ (show err)
-              Left err   -> putStrLn $ "Parsing Error: " ++ (show err)
-     
+main = do
+  putStr "user> "
+  let ctx = mkContext
+  parseTree <- fmap (runParser parseTerm ctx "simpleTyped") getLine
+  case parseTree of
+    Right expr ->
+      case (typeOf ctx expr) of
+        Right ex ->
+          putStrLn $
+          "Evaluating " ++
+          (show parseTree) ++ "\n=> " ++ (showTerm ctx . eval) expr
+        Left err -> putStrLn $ "Type Error: " ++ (show err)
+    Left err -> putStrLn $ "Parsing Error: " ++ (show err)
